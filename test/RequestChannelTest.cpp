769dbb5b47be780b239976efd70220140f893e11
Scott Ragan <ragansa@fb.com>
2017-09-22 14:59:49 -0700

    drop passed value on the floor

--- test/RequestChannelTest.cpp
+++ test/RequestChannelTest.cpp
@@ -103,9 +103,7 @@ class TestChannelResponder : public rsocket::RSocketResponder {
     // add initial payload to testSubscriber values list
     testSubscriber_->onNext(initialPayload.moveDataToString());
 
-    requestStream->map([](auto p) { return p.data->cloneCoalescedAsValue()
-                .moveToFbString()
-                .toStdString(); })
+    requestStream->map([](auto p) { return std::string("ay"); })
         ->subscribe(testSubscriber_);
 
     return Flowables::range(1, rangeEnd_)->map([&](int64_t v) {
